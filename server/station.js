14f1846faecc9c43c89ca40594758e2f241ba5fd
Jono Cooper <jono@jonocooper.com>
2016-08-17 11:53:08 +1200

    Station API returning trips information

--- server/station.js
+++ server/station.js
@@ -1,6 +1,9 @@
 var fs = require('fs')
 var request = require('request')
 var moment = require('moment-timezone')
+var azure = require('azure-storage')
+
+var tableSvc = azure.createTableService();
 
 var options = {
   url: 'https://api.at.govt.nz/v2/gtfs/stopTimes/stopId/',
@@ -35,23 +38,52 @@ var station = {
         var time = moment().tz('Pacific/Auckland')
         var currentTime = new Date(Date.UTC(1970,0,1,time.hour(),time.minute())).getTime()/1000
 
-        var filteredTrips = []
+        var query = new azure.TableQuery()
+
+        var filteredTrips = {}
+        var filteredTripsLength = 0
         var trips = JSON.parse(body).response
         trips.forEach(function(trip) {
           if (trip.arrival_time_seconds < (currentTime + 7200) && trip.arrival_time_seconds > (currentTime - 1200)) {
-            filteredTrips.push({
-              trip_id: trip.trip_id,
+            
+            if (filteredTripsLength === 0) {
+              query.where('RowKey eq ?', trip.trip_id)
+            } else {
+              query.or('RowKey eq ?', trip.trip_id)
+            }
+
+            filteredTrips[trip.trip_id] = {
               arrival_time: trip.arrival_time,
               stop_sequence: trip.stop_sequence
-            })
+            }
+            filteredTripsLength++
           }
         })
 
         var sending = JSON.parse(JSON.stringify(station.stops[req.params.station]))
         sending.currentTime = currentTime
-        sending.trips = filteredTrips
         
-        res.send(sending)
+        tableSvc.queryEntities('trips',query, null, function(error, result, response) {
+          if (error) throw error
+          // query was successful
+          result.entries.forEach(function(trip) {
+            var k = trip.RowKey._
+            filteredTrips[k].route_long_name = trip.route_long_name._
+            filteredTrips[k].agency_id = trip.agency_id._
+            filteredTrips[k].direction_id = trip.direction_id._
+            filteredTrips[k].end_date = trip.end_date._
+            filteredTrips[k].frequency = trip.frequency._
+            filteredTrips[k].route_short_name = trip.route_short_name._
+            filteredTrips[k].route_type = trip.route_type._
+            filteredTrips[k].start_date = trip.start_date._
+            filteredTrips[k].trip_headsign = trip.trip_headsign._
+          })
+
+          sending.trips = filteredTrips
+          res.send(sending)
+        });
+        
+        
       })
       
     } else {
