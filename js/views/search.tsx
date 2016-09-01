b4c51fc5a3abc254ca65367fc5a6cf8da8b42969
Jono Cooper <jono@jonocooper.com>
2016-09-02 02:44:51 +1200

    Add Leaflet Map Library
    Thanks matt you found a great library :)

--- js/views/search.tsx
+++ js/views/search.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react'
 import { browserHistory } from 'react-router'
 import { StationStore } from '../stores/stationStore.ts'
+
+declare function require(name: string): any;
+let leaflet = require('react-leaflet')
+let Map = leaflet.Map
+let Marker = leaflet.Marker
+let Popup = leaflet.Popup
+let TileLayer = leaflet.TileLayer
+
+console.log(leaflet)
+
 interface IAppProps extends React.Props<Search> {}
 
 interface IAppState {
@@ -27,10 +37,23 @@ class Search extends React.Component<IAppProps, IAppState> {
     browserHistory.push(`/s/${this.state.station}`)
   }
   public render() {
+    const position = [-36.844229, 174.767823];
+
     return (
       <div>Add station using this input thing<br />
         <input type="text" placeholder="station number" onChange={this.triggerChange} />
         <button onClick={this.triggerClick}>search</button>
+        <Map center={position} zoom={17} touchZoom={true} className="map">
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>This is Britomart I think</span>
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     )
   }
