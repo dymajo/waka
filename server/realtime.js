46a8c98116406f17f0bb006b4c15b37334d6cc4e
Matt Davidson <matt@mattdavidson.kiwi>
2016-09-23 15:05:03 +1200

    Complete list of Double Decker Buses

--- server/realtime.js
+++ server/realtime.js
@@ -12,19 +12,23 @@ var isDoubleDecker = function(vehicle) {
   var doubleDeckers = [
     // NZ Bus / Metrolink
     // Alexander Dennis Enviro500
-    '3A99', '3A9A', '3A9B', '3A9C', '3A9D', '3A9E', '3A9F',
-    '3AA0', '3AA1', '3AA2', '3AA3', '3AA4', '3AA5', '3AA6',
-    '3AA7', '3AA8', '3AA9', '3AAA', '3AAB', '3AAC', '3AAD',
-    '3AAE', '3AAF',
+
+    '3A99', '3A9A', '3A9B', '3A9C', '3A9D', '3A9E', '3A9F', 
+    '3AA0', '3AA1', '3AA2', '3AA3', '3AA4', '3AA5', '3AA6', 
+    '3AA7', '3AA8', '3AA9', '3AAA', '3AAB', '3AAC', '3AAD', 
+    '3AAE', '3AAF', 
+
 
     // Howick and Eastern
     // More Enviro500
-    '5FB5', '5FB6', '5FB7',
-    '5FB9',
-    '5FBB',
+    '5FB4', '5FB5', '5FB6', '5FB7', '5FB8', '5FB9', '5FBA',
+    '5FBB', '5FBC', '5FBD', '5FBE', '5FBF', '5FC0', '5FC1',
+    '5FC2',
 
-    //NEX no name buses
-    '5625', '5626', '5627'
+    //NEX BCI CityRider FBC6123BRZ
+    '5622', '5623', '5624', '5625', '5626', '5627', '5628',
+    '5629', '562A', '562B', '562C', '562D', '562E', '562F',
+    '5630'
   ]
   // uncomment this line if you want it to randomly be a double decker
   // if (Math.ceil(Math.random()*2) >1) {
