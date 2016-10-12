fae95c44e661d70da3bcdb7a08de6b75e2261033
Jono Cooper <jono@jonocooper.com>
2016-10-13 10:30:16 +1300

    Add List of Lines, and more Advanced Exception Checking

--- js/views/lines.tsx
+++ js/views/lines.tsx
@@ -10,15 +10,21 @@ let request = require('reqwest')
 interface IAppProps extends React.Props<Lines>{
 }
 
+interface LineData {
+    [key: string]: Array<Array<string>> 
+}
+
 interface IAppState{
-    service?: string
+    service?: string,
+    allLines?: LineData
 }
 
 class Lines extends React.Component<IAppProps, IAppState>{
     constructor(props){
         super(props)
-        this.state={
-            service: ""
+        this.state = {
+            service: '',
+            allLines: undefined
         }
         this.triggerChange = this.triggerChange.bind(this)
     }
@@ -35,14 +41,39 @@ class Lines extends React.Component<IAppProps, IAppState>{
         } as IAppState)
     }
 
+    public componentDidMount() {
+        request(`/a/lines`).then((res)=>{
+            console.log(res)
+            this.setState({
+                allLines: res
+            })           
+        })
+    }
+
 
     public render() {
+        var ret
+        // there needs to be a sorting function in here probably
+        if (this.props.children === null) {
+            ret = []
+            for (var key in this.state.allLines) {
+                var el = this.state.allLines[key]
+                if (el[0].length === 1) {
+                    ret.push(<div key={key}><Link to={'/l/'+key}><strong>{key}</strong> {el[0][0]}</Link></div>)
+                } else {
+                    ret.push(<div key={key}><Link to={'/l/'+key}><strong>{key}</strong> {el[0][0]} to {el[0][1]}</Link></div>)
+                }
+                
+            }
+        } else {
+            ret = this.props.children
+        }
         return(
             <div>
                 <input value={this.state.service} type="text" placeholder="Enter Service Name" onChange={this.triggerChange} /><br/>
                 <button onClick={this.viewLine(this.state.service)}>View Line</button>
                 Your line: 
-                {this.props.children}
+                {ret}
                 <SearchSwitch />
             </div>
         )
--- server/line.js
+++ server/line.js
@@ -13,7 +13,34 @@ var shapeWKBOptions = {
     }
 }
 
+var allLines = {
+    // TRAINS
+    'STH': [['Britomart Train Station', 'Papakura Train Station']],
+    'WEST': [['Britomart Train Station', 'Swanson Train Station']],
+
+    // CITY
+    'CTY': [['City Link', 'Wynyard Quarter', 'Greys Ave']],
+    'INN': [['Inner Link Clockwise'],['Inner Link Anticlockwise']],
+
+    // ISTHMUS
+    'OUT': [['Outer Link Clockwise'],['Outer Link Anticlockwise']],
+    '007': [['Pt Chevalier', 'St Heliers', 'Hospital And Selwyn Village'], ['Pt Chevalier', 'St Heliers', 'Selwyn Village And Hospital']],
+    '233': [['Midtown', 'New Lynn', 'Sandringham Road and St Lukes']],
+    '255': [['Civic Centre', 'May Rd'], ['Civic Centre', 'May Rd', 'Flyover']],
+    '258': [['Civic Centre', 'Blockhouse Bay'], ['Civic Centre', 'Blockhouse Bay', 'Flyover']],
+    '267': [['Civic Centre', 'Lynfield'], ['Civic Centre', 'Lynfield', 'Flyover']],
+    '274': [['Britomart', 'Three Kings']],
+    '277': [['Britomart', 'Waikowhai']],
+
+    // SOUTH
+    '500': [['Britomart', 'Mission Heights', 'Botany Town Centre']]
+}
+
 var line = {
+    getLines: function(req, res) {
+        res.send(allLines)
+    },
+
     getLine: function(req, res) {
         var query = new azure.TableQuery()
             .where('route_short_name eq ?', req.params.line)
@@ -21,9 +48,24 @@ var line = {
             if (err) {
                 return reject(err)
             }
+            var versions = {}
             var results = []
             result.entries.forEach(function(route){
                 if (line.exceptionCheck(route) === true ){
+                    var v = parseFloat(route.RowKey._.split('_v').slice(-1)[0])
+                    if (typeof(versions[route.route_long_name._]) === 'undefined') {
+                        versions[route.route_long_name._] = v 
+                    } else if (v > versions[route.route_long_name._]) {
+                        versions[route.route_long_name._] = v
+                    }
+                }
+            })
+            result.entries.forEach(function(route){
+                if (typeof(versions[route.route_long_name._]) === 'undefined') {
+                    return
+                }
+                if (versions[route.route_long_name._] === parseFloat(route.RowKey._.split('_v').slice(-1)[0])) {
+                    versions[route.route_long_name._] = null // zero it so we can't have stuff with the same version_id
                     results.push({
                         route_id: route.RowKey._,
                         route_long_name: route.route_long_name._,
@@ -51,21 +93,41 @@ var line = {
     },
 
     exceptionCheck: function(route){
+        // blanket thing for no schools
         if (route.trip_headsign._ === "Schools"){
             return false
         }
-        else if (route.route_short_name._ === "INN"){
-            if (route.route_long_name._ !== "Inner Link Anticlockwise" && route.route_long_name._ !== "Inner Link Clockwise") {
-                return false
-            }
-        } else if (route.route_short_name._ === "OUT") {
-            if (route.route_long_name._ !== "Outer Link Anticlockwise" && route.route_long_name._ !== "Outer Link Clockwise") {
-                return false
-            }
+        if (typeof(allLines[route.route_short_name._]) === 'undefined') {
+            return true
         }
-
-
-        return true
+        var retval = false
+        allLines[route.route_short_name._].forEach(function(variant) {
+            if (variant.length === 1 && route.route_long_name._ === variant[0]) {
+                retval = true
+            // normal routes - from x to x
+            } else if (variant.length === 2) {
+                var splitName = route.route_long_name._.toLowerCase().split(' to ')
+                if (variant[0].toLowerCase() == splitName[0] && variant[1].toLowerCase() == splitName[1]) {
+                    retval = true
+                // reverses the order
+                } else if (variant[1].toLowerCase() == splitName[0] && variant[0].toLowerCase() == splitName[1]) {
+                    retval = true
+                }
+            // handles via Flyover or whatever
+            } else if (variant.length === 3) {
+                var splitName = route.route_long_name._.toLowerCase().split(' to ')
+                if (splitName[1].split(' via ')[1] === variant[2].toLowerCase()) {
+                    splitName[1] = splitName[1].split(' via ')[0]
+                    if (variant[0].toLowerCase() === splitName[0] && variant[1].toLowerCase() === splitName[1]) {
+                        retval = true
+                    // reverses the order
+                    } else if (variant[1].toLowerCase() === splitName[0] && variant[0].toLowerCase() === splitName[1]) {
+                        retval = true
+                    }
+                }
+            }
+        })
+        return retval
 
     }
 }
