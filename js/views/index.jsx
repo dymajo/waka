69b45289228e9014d357163f535738acd4418d3f
Jono Cooper <jono@jonocooper.com>
2016-12-14 22:32:46 +1300

    Slideable Map Prototype

--- js/views/index.jsx
+++ js/views/index.jsx
@@ -5,6 +5,19 @@ import { StationStore, StationMap } from '../stores/stationStore.js'
 import { UiStore } from '../stores/uiStore.js'
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mapView: false
+    }
+
+    this.toggleStations = this.toggleStations.bind(this)
+  }
+  toggleStations() {
+    this.setState({
+      mapView: !this.state.mapView
+    })
+  }
   render() {
     // I hate myself for doing this, but iOS scrolling is a fucking nightmare
     var className = 'panes'
@@ -15,20 +28,31 @@ class Index extends React.Component {
     if (window.navigator.standalone) {
       className += ' ios-standalone'
     }
+    let rootClassName = 'root-container'
+    if (this.state.mapView) {
+      rootClassName += ' map-view'
+    }
     return (
       <div className={className}>
-        <header className="material-header">
-          <div>
-            <h1 className="full-height">
-              <img className="logo" src='/icons/icon.png' width='16' />
-              <strong>DYMAJO</strong> <span>Transit</span></h1>
+        <div className={rootClassName}>
+          <header className="material-header">
+            <div>
+              <h1 className="full-height">
+                <img className="logo" src='/icons/icon.png' width='16' />
+                <strong>DYMAJO</strong> <span>Transit</span></h1>
+            </div>
+          </header>
+          <div className="root-map">
+            Root Map.
           </div>
-        </header>
-        <div className="root-map">
-          Root Map.
-        </div>
-        <div className="root-card">
-          Root Card.
+          <div className="root-card">
+            <div className="root-card-bar">
+              <button onClick={this.toggleStations}>Stations</button>
+              <button>Lines</button>
+              <button>Alerts</button>
+            </div>
+            hi hi hi
+          </div>
         </div>
         <div className="content">
         {this.props.children}
