a734dfb2882c7852224d19ba93a479fd67ac78b2
Jono Cooper <jono@jonocooper.com>
2016-12-17 16:05:59 +1300

    Logic for Scroll Rejection

--- js/views/index.jsx
+++ js/views/index.jsx
@@ -11,12 +11,60 @@ class Index extends React.Component {
       mapView: false
     }
 
+    this.touchstartpos = null
+    this.scrolllock = false
+
     this.toggleStations = this.toggleStations.bind(this)
+    this.triggerTouchStart = this.triggerTouchStart.bind(this)
+    this.triggerTouchMove = this.triggerTouchMove.bind(this)
+    this.triggerScroll = this.triggerScroll.bind(this)
   }
   toggleStations() {
     this.setState({
       mapView: !this.state.mapView
     })
+  }
+  triggerTouchStart(e) {
+    // only start the pull down if they're at the top of the card
+    if (this.refs.touchcard.scrollTop === 0) {
+      // console.log('touchstart', e.touches[0].clientY)
+      this.touchstartpos = e.touches[0].clientY
+      this.scrolllock = null
+    } else {
+      this.touchstartpos = null
+      this.scrolllock = null
+    }
+  }
+  triggerTouchMove(e) {
+    // cancels if they're not at the top of the card
+    if (this.touchstartpos === null) {
+      return
+    }
+
+    if (this.scrolllock === true) {
+      console.log('scrolling locked')
+      e.preventDefault()
+      return
+    } else if (this.scrolllock === false) {
+      console.log('scrolling enabled')
+      return
+    }
+
+    // if (this.refs.touchcard.scrollTop === 0) {
+    let newPos = e.changedTouches[0].clientY
+    if (this.touchstartpos < newPos) {
+      this.scrolllock = true
+    } else {
+      this.scrolllock = false
+    }
+    // }
+  }
+  triggerScroll(e) {
+    // if (this.scrolllock === true) {
+    //   // console.log('scroll should be locked')
+    //   e.preventDefault()
+    //   // return false
+    // }
   }
   render() {
     // I hate myself for doing this, but iOS scrolling is a fucking nightmare
@@ -45,13 +93,35 @@ class Index extends React.Component {
           <div className="root-map">
             Root Map.
           </div>
-          <div className="root-card">
+          <div className="root-card"
+            ref="touchcard"
+            onTouchStart={this.triggerTouchStart}
+            onTouchMove={this.triggerTouchMove}
+            onScroll={this.triggerScroll}
+          >
             <div className="root-card-bar">
               <button onClick={this.toggleStations}>Stations</button>
               <button>Lines</button>
               <button>Alerts</button>
             </div>
-            hi hi hi
+            <div className="root-card-content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat.  
+            </div>
           </div>
         </div>
         <div className="content">
