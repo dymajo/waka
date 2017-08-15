8a2386b3ffb2d6b110853e8a925f4117f178198f
Jono Cooper <jono@jonocooper.com>
2017-08-15 16:41:02 +1200

    iOS Fixes

--- js/stores/settingsStore.js
+++ js/stores/settingsStore.js
@@ -12,7 +12,7 @@ export class settingsStore {
         this.state[attrname] = preState[attrname]
       }
     }
-    localStorage.setItem('AppVersion', '1.2.0')
+    localStorage.setItem('AppVersion', '1.3.0')
   }
   getState() {
     return this.state
