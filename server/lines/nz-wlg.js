79c178b059b4d9dd27f0f0d89c8b5f0dfbfff7a1
Jono Cooper <jono@jonocooper.com>
2017-09-17 22:01:35 +1200

    Add All Colors

--- server/lines/nz-wlg.js
+++ server/lines/nz-wlg.js
@@ -15,12 +15,113 @@ const regionEnum = {
 
 const lineColors = {}
 const lineColorizer = (agency, route_short_name) => {
-  switch (agency) {
-  case 'WCCL':
-    return '#ff0000'
-  default:
-    return '#bada55'
+  const ag_mapper = {
+    'WCCL': 'e43e42',
+    'EBYW': '41bada',
   }
+  const rt_mapper = {
+    'HVL': 'e52f2b',
+    'JVL': '4f9734',
+    'KPL': 'f39c12',
+    'MEL': '21b4e3',
+    'WRL': 'e52f2b',
+    '5': 'ff602d',
+    '6': '00a07c',
+    '9': 'ff3818',
+    '13': 'be5816',
+    '14': '80a9bd',
+    '20': 'fa8ec3',
+    '21': '6a741d',
+    '24': '19382c',
+    '25': '09314f',
+    '28': 'fdb02d',
+    '29': '00768d',
+    '47': 'a86419',
+    '50': '0b8c5a',
+    '52': '5d9734',
+    '53': 'f76a1f',
+    '54': 'd82574',
+    '55': '80311c',
+    '56': 'ff7b27',
+    '57': '0e7c8a',
+    '58': 'd82574',
+    '83': 'cf3113',
+    '91': 'ff9430',
+    '110': '914aa5',
+    '111': '0068a7',
+    '112': '72d8f7',
+    '114': 'b7bb33',
+    '115': '006a49',
+    '120': '54bc49',
+    '121': '0068a7',
+    '130': '00b8f1',
+    '145': '00768d',
+    '150': 'ac1846',
+    '154': 'fc529b',
+    '160': 'fa2b2e',
+    '170': '968a19',
+    '200': 'fc529b',
+    '201': '007a87',
+    '202': 'ffb52f',
+    '203': '6f85c5',
+    '204': 'b4d099',
+    '205': '72d8f7',
+    '206': 'f02a63',
+    '210': '751506',
+    '211': 'ff7233',
+    '220': '258d52',
+    '226': '1e83b6',
+    '230': 'e6273f',
+    '236': '7f2479',
+    '250': '006a9f',
+    '251': '0068a7',
+    '260': '4b155e',
+    '261': 'ff2d16',
+    '262': '87b366',
+    '263': '0068a7',
+    '270': 'ff6920',
+    '280': '0b349b',
+    '289': '0068a7',
+    '290': '00afe7',
+    '300': '0068a7',
+    'N1': 'eb9c4f',
+    'N2': '824f9a',
+    'N3': 'ed8dbd',
+    'N4': '86a7ba',
+    'N5': 'b42d72',
+    'N6': '065880',
+    'N8': '36bac9',
+    'N22': '4fad55',
+    'N66': '909543',
+    'N88': 'ad3e35',
+  }
+  if (['1', '4', '32'].indexOf(route_short_name) > -1) {
+    return '#9b2994'
+  } else if (['2', '31'].indexOf(route_short_name) > -1) {
+    return '#fa2b2e'
+  } else if (['3', '3S', '3W', '17'].indexOf(route_short_name) > -1) {
+    return '#74c447'
+  } else if (['7', '8'].indexOf(route_short_name) > -1) {
+    return '#df6c1d'
+  } else if (['10', '30', '11'].indexOf(route_short_name) > -1) {
+    return '#80311c'
+  } else if (['18', '43', '44', '45', '46'].indexOf(route_short_name) > -1) {
+    return '#3ac1ee'
+  } else if (['22', '23'].indexOf(route_short_name) > -1) {
+    return '#ff8d26'
+  } else if (['54', '55'].indexOf(route_short_name) > -1) {
+    return '#d82574'
+  } else if (['70', '80'].indexOf(route_short_name) > -1) {
+    return '#002a49'
+  } else if (['81', '84', '85'].indexOf(route_short_name) > -1) {
+    return '#c03222'
+  } else if (['90', '92', '93'].indexOf(route_short_name) > -1) {
+    return '#a28875'
+  } else if (['97', '97H', '97N'].indexOf(route_short_name) > -1) {
+    return '#139fda'
+  }
+  let retValue = rt_mapper[route_short_name] || ag_mapper[agency] || '000'
+  return '#' + retValue
 }
 
 const lineGroups = [{
@@ -28,7 +129,7 @@ const lineGroups = [{
   items: [],
 },
 {
-  name: 'Bus',
+  name: 'Central',
   items: [],
 },
 {
