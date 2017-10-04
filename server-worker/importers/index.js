3d9a243fc9abd22bb76219d955e141b9c7589206
Jono Cooper <jono@jonocooper.com>
2017-10-04 18:51:43 +1300

    Shapes Importer

--- server-worker/importers/index.js
+++ server-worker/importers/index.js
@@ -1,17 +1,51 @@
 const fs = require('fs')
+const path = require('path')
+const rimraf = require('rimraf')
 const request = require('request')
 const log = require('../../server-common/logger.js')
 const gtfsImport = require('../db/gtfs-import.js')
+const createShapes = require('../db/create-shapes.js')
 
 class Importer {
+  constructor() {
+    this.importer = new gtfsImport()
+    this.current = require('./' + global.config.prefix + '.js')
+  }
   async start() {
-    const current = require('./' + global.config.prefix + '.js')
-    const importer = new gtfsImport()
     // await this.download()
-    await importer.unzip(current.zipLocation)
-    for (let file of current.files) {
-      await importer.upload(current.zipLocation + 'unarchived', file, global.config.version, file.versioned)
+    await this.importer.unzip(this.current.zipLocation)
+    await this.db()
+    await this.shapes()
+  }
+  async db() {
+    for (let file of this.current.files) {
+      await this.importer.upload(this.current.zipLocation + 'unarchived', file, global.config.version, file.versioned)
     }
+  }
+  async shapes() {
+    const creator = new createShapes()
+    const inputDir = path.resolve(this.current.zipLocation + 'unarchived', 'shapes.txt')
+    const outputDir = path.resolve(this.current.zipLocation + 'unarchived', 'shapes')
+    const outputDir2 = path.resolve(outputDir, global.config.version)
+
+    // make sure the old output dir exists
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir)
+    }
+
+    // cleans up old import if exists
+    if (fs.existsSync(outputDir2)) {
+      await new Promise((resolve, reject) => {
+        rimraf(outputDir2, resolve)
+      })
+    }    
+    fs.mkdirSync(outputDir2)
+
+    // creates the new datas
+    await creator.create(inputDir, outputDir, [global.config.version])
+
+    const containerName = (global.config.prefix + '-' + global.config.version).replace('.', '-').replace('_', '-')
+    await creator.upload(containerName, path.resolve(outputDir, global.config.version))
   }
   download() {
     return new Promise((resolve, reject) => {
