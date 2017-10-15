2720be26297063fc67c27882581bb80421e9e239
Jono Cooper <jono@jonocooper.com>
2017-10-16 07:02:26 +1300

    Fix Body Parser

--- server-static/index.js
+++ server-static/index.js
@@ -1,12 +1,14 @@
 const colors = require('colors')
 const express = require('express')
 const router = require('./router.js')
+const bodyParser = require('body-parser')
 
 const log = require('../server-common/logger.js')
 
 log('Static Server Started')
 
 const app = express()
+app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.setHeader('X-Powered-By', 'dymajo-transit-static')
   next()
