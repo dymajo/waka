e970dc78a748534372e93761ecfd48d4a0add391
Jono Cooper <jono@jonocooper.com>
2018-04-29 22:42:09 +1200

    Start of Apple Maps Style UI

---
+++ js/views/shell/content.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withRouter, Switch, Route } from 'react-router-dom'
+import { TransitionGroup, Transition } from 'react-transition-group'
+
+import Lines from '../lines.jsx'
+
+class Content extends React.Component {
+  static propTypes = {
+    rootComponent: PropTypes.func,
+  }
+  render() {
+    return (
+      <Switch location={this.props.location} key={this.props.location.key}>
+        <Route path="/" exact render={this.props.rootComponent} />
+        <Route path="/l/:region" component={Lines} />
+      </Switch>
+    )
+  }
+}
+const ContentView = withRouter(Content)
+export { ContentView }
