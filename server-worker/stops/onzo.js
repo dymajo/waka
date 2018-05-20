fe2b0eb943cc2a206fbd783e2ad661ffc62fde36
Matt Davidson <matt@mattdavidson.kiwi>
2018-05-20 15:55:02 +1200

    return onzos

--- server-worker/stops/onzo.js
+++ server-worker/stops/onzo.js
@@ -4,27 +4,28 @@ const onzos = [{ onzo: { bike: 1 } }]
 
 const onzo = {
   getBikes(lat, lon, dis) {
-    const options = {}
-    options.url = `https://app.onzo.co.nz/nearby/${lat}/${lon}/${dis}`
-    options.json = true
-    Request(options, (error, response, body) => {
-      if (error) {
-        return
-      }
-      body.data.map(onzo => {
-        onzos.push({
-          stop_id: onzo.iccid,
-          stop_lat: onzo.latitude,
-          stop_lon: onzo.longitude,
-          stop_lng: onzo.longitude,
-          stop_region: 'nz-akl',
-          route_type: -2,
-          stop_name: 'Onzo Bike',
-          battery: onzo.battery,
-          updated: onzo.updateTime,
-        })
+    return new Promise((resolve, reject) => {
+      const options = {}
+      options.url = `https://app.onzo.co.nz/nearby/${lat}/${lon}/${dis}`
+      options.json = true
+      Request(options, (error, response, body) => {
+        if (error) {
+          return
+        }
+        resolve(
+          body.data.map(onzo => ({
+            stop_id: onzo.iccid,
+            stop_lat: onzo.latitude,
+            stop_lon: onzo.longitude,
+            stop_lng: onzo.longitude,
+            stop_region: 'nz-akl',
+            route_type: -2,
+            stop_name: 'Onzo Bike',
+            battery: onzo.battery,
+            updated: onzo.updateTime,
+          }))
+        )
       })
-      return Promise.resolve(onzos)
     })
   },
 }
