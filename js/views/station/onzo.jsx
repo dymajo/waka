890dc4c86e66f5f27a104d29c4d8dc8f3808fd67
Matt Davidson <matt@mattdavidson.kiwi>
2018-05-20 19:32:26 +1200

    upgrade onzo to rnw

--- js/views/station/onzo.jsx
+++ js/views/station/onzo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native-web'
 
 import moment from 'moment-timezone'
 
@@ -8,10 +9,14 @@ class Onzo extends React.Component {
   }
   render() {
     const updatedgmt = moment.tz(this.props.updated, 'Etc/GMT')
-    const updated = updatedgmt.format('hh:mm a DD/MM/YYYY')
+    const updated = updatedgmt.format('hh:mm a [-] DD/MM/YYYY')
     return (
       <div style={{ textAlign: 'center' }}>
-        Bike last seen: {updated}
+        <View style={styles.onzoButton}>
+          <Text style={styles.onzoHeader}>Last Seen</Text>
+          <Text style={styles.onzoUpdated}>{updated}</Text>
+        </View>
+
         <a
           className="nice-button secondary"
           href="https://twitter.com/search?q=onzo"
@@ -24,5 +29,17 @@ class Onzo extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  onzoButton: {
+    backgroundColor: '#FFCC00',
+    textAlign: 'left',
+    padding: '10px',
+    marginBottom: '20px',
+    borderTop: '1px solid rgba(0,0,0,0.25)',
+  },
+  onzoHeader: { color: 'black', fontSize: '36px', fontWeight: 600 },
+  onzoUpdated: {},
+})
 
 export default Onzo
