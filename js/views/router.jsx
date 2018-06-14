e7e69423702199089296356642c40f8333224b7a
Jono Cooper <jono@jonocooper.com>
2018-06-15 00:40:18 +1200

    Speed Improvements
    Approx 1/3rd Speedup - 150ms to 100ms. Still too slow!

--- js/views/router.jsx
+++ js/views/router.jsx
@@ -14,49 +14,28 @@ class RouterRender extends React.Component {
   static propTypes = {
     location: PropTypes.object,
   }
-  triggerStateUpdate = key => {
-    return node => {
-      if (node) {
-        if (key === 'entered') {
-          UiStore.state.exiting = window.location.pathname
-        }
-        UiStore.trigger('animation', [key, node])
-      }
-    }
-  }
   render() {
     return (
-      <TransitionGroup className="content">
-        <Transition
-          timeout={300}
-          key={this.props.location.key}
-          onEntering={this.triggerStateUpdate('entering')}
-          onEntered={this.triggerStateUpdate('entered')}
-          onExiting={this.triggerStateUpdate('exiting')}
-          onExited={this.triggerStateUpdate('exited')}
-        >
-          <Switch location={this.props.location} key={this.props.location.key}>
-            <Route path="/" exact />
+      <Switch location={this.props.location} key={this.props.location.key}>
+        <Route path="/" exact />
 
-            <Route
-              path="/s/:region/:station/realtime/:trip_id"
-              component={VehicleLocationBootstrap}
-            />
-            <Route
-              path="/s/:region/:station/timetable/:route_name"
-              component={Timetable}
-            />
+        <Route
+          path="/s/:region/:station/realtime/:trip_id"
+          component={VehicleLocationBootstrap}
+        />
+        <Route
+          path="/s/:region/:station/timetable/:route_name"
+          component={Timetable}
+        />
 
-            <Route
-              path="/l/:region/:line_id"
-              component={VehicleLocationBootstrap}
-            />
+        <Route
+          path="/l/:region/:line_id"
+          component={VehicleLocationBootstrap}
+        />
 
-            <Route path="/settings" component={Settings} />
-            <Route path="/testlines" component={TestLines} />
-          </Switch>
-        </Transition>
-      </TransitionGroup>
+        <Route path="/settings" component={Settings} />
+        <Route path="/testlines" component={TestLines} />
+      </Switch>
     )
   }
 }
