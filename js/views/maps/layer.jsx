2da349d34a873ce98c647d646978cd35855ccd6b
Jono Cooper <jono@jonocooper.com>
2018-07-02 23:58:42 +1200

    Basic Points Layer

--- js/views/maps/layer.jsx
+++ js/views/maps/layer.jsx
@@ -1,12 +1,15 @@
 import leaflet from 'leaflet'
 const GeoJSON = leaflet.geoJSON
+const DivIcon = leaflet.divIcon
+const Marker = leaflet.marker
+const CircleMarker = leaflet.circleMarker
 
 import { UiStore } from '../../stores/uiStore.js'
 
 export class Layer {
   features = []
   visible = false
-  show(bounds = null) {
+  show(bounds = null, dispose = true, maxZoom = -1) {
     if (bounds !== null) {
       const options = {}
       if (document.documentElement.clientWidth <= 850) {
@@ -18,23 +21,58 @@ export class Layer {
       )
     }
     if (this.visible === true) return
+    if (maxZoom > -1 && dispose === true) {
+      this.maxZoom = maxZoom
+      UiStore.basemap.on('zoomend', this.toggleOnZoom)
+      return this.toggleOnZoom()
+    }
     this.visible = true
     this.features.forEach(feature => {
       feature.addTo(UiStore.basemap)
     })
   }
-  hide() {
+  hide(dispose = true) {
     if (this.visible === false) return
+    if (this.maxZoom > -1 && dispose === true) {
+      UiStore.basemap.off('zoomend', this.toggleOnZoom)
+    }
     this.visible = false
     this.features.forEach(feature => {
       feature.remove(UiStore.basemap)
     })
   }
+  toggleOnZoom = () => {
+    // this is pretty primative. need some way of checking the spacing between items
+    if (this.maxZoom <= UiStore.basemap.getZoom()) {
+      this.show(null, false)
+    } else {
+      this.hide(null, false)
+    }
+  }
   add(type, data, props = {}) {
-    const feature = GeoJSON(data, props)
-    this.features.push(feature)
-    if (this.visible === true) {
-      feature.addTo(UiStore.basemap)
+    let feature = null
+    if (type === 'geojson') {
+      if (props.typeExtension === 'CircleMarker') {
+        props.pointToLayer = function(feature, latlng) {
+          return CircleMarker(latlng, props.typeExtensionOptions)
+        }
+      } else if (props.typeExtension === 'InvisibleMarker') {
+        const divIcon = DivIcon({
+          iconSize: [48, 48],
+          className: 'invisible-icon',
+        })
+        props.typeExtensionOptions.icon = divIcon
+        props.pointToLayer = function(feature, latlng) {
+          return Marker(latlng, props.typeExtensionOptions)
+        }
+      }
+      feature = GeoJSON(data, props)
+    }
+    if (feature !== null) {
+      this.features.push(feature)
+      if (this.visible === true) {
+        feature.addTo(UiStore.basemap)
+      }
     }
   }
 }
