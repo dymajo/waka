7db71e6c4a7dd7ca4fab69ffea1bb9b97c8698ec
Jono Cooper <jono@jonocooper.com>
2018-07-22 20:59:01 +1200

    Fix #123: Wellington Headsign (#127)
    * Fix Wellington Headsign
    
    * Reduce Importer Duplication

--- server-worker/importers/au-syd.js
+++ server-worker/importers/au-syd.js
@@ -1,66 +1,10 @@
-const path = require('path')
-const request = require('request')
-const fs = require('fs')
-const log = require('../../server-common/logger.js')
-var gtfsDownloadOptions = {
-  url: 'https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete/gtfs',
-  headers: {
-    'Authorization': process.env.nswApiKey
-  }
-}
+const defaultConfig = require('./defaultConfig.js')
 
-const sydney = {
-  zipLocation: path.join(__dirname, '../../cache/sydney.zip'),
-  files: [
-    { //tick
-      name: 'agency.txt',
-      table: 'agency',
-      versioned: false,
-    },
-    {
-      name: 'stops.txt',
-      table: 'stops',
-      versioned: false,
-    },
-    { //tick
-      name: 'routes.txt',
-      table: 'routes',
-      versioned: false,
-    },
-    {
-      name: 'trips.txt',
-      table: 'trips',
-      versioned: false,
-    },
-    {
-      name: 'stop_times.txt',
-      table: 'stop_times',
-      versioned: false,
-    },
-    { //tick
-      name: 'calendar.txt',
-      table: 'calendar',
-      versioned: false,
-    },
-    { //tick
-      name: 'calendar_dates.txt',
-      table: 'calendar_dates',
-      versioned: false,
-    },
-  ],
-  shapeFile: 'shapes.txt', //tick
-  download: () => {
-    return new Promise((resolve, reject) => {
-      log('Downloading GTFS Data from TfNSW')
-      log(process.env.nswApiKey)
-      const newOpts = JSON.parse(JSON.stringify(gtfsDownloadOptions))
-      const gtfsRequest = request(newOpts).pipe(fs.createWriteStream(sydney.zipLocation))
-      gtfsRequest.on('finish', function() {
-        log('Finished Downloading GTFS Data')
-        resolve()
-      })
-      gtfsRequest.on('error', reject)
-    })
-  }
-}
+const sydney = defaultConfig.get('sydney', {
+  url:
+    'https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete/gtfs',
+  headers: {
+    Authorization: process.env.nswApiKey,
+  },
+})
 module.exports = sydney
--- server-worker/importers/nz-otg.js
+++ server-worker/importers/nz-otg.js
@@ -1,59 +1,6 @@
-const path = require('path')
-const request = require('request')
-const fs = require('fs')
-const log = require('../../server-common/logger.js')
+const defaultConfig = require('./defaultConfig.js')
 
-const otago = {
-  zipLocation: path.join(__dirname, '../../cache/otago.zip'),
-  files: [
-    {
-      name: 'agency.txt',
-      table: 'agency',
-      versioned: false,
-    },
-    {
-      name: 'stops.txt',
-      table: 'stops',
-      versioned: false,
-    },
-    {
-      name: 'routes.txt',
-      table: 'routes',
-      versioned: false,
-    },
-    {
-      name: 'trips.txt',
-      table: 'trips',
-      versioned: false,
-    },
-    {
-      name: 'stop_times.txt',
-      table: 'stop_times',
-      versioned: false,
-    },
-    {
-      name: 'calendar.txt',
-      table: 'calendar',
-      versioned: false,
-    },
-    {
-      name: 'calendar_dates.txt',
-      table: 'calendar_dates',
-      versioned: false,
-    },
-  ],
-  shapeFile: 'shapes.txt',
-  download: () => {
-    return new Promise((resolve, reject) => {
-      const url = 'https://www.orc.govt.nz/media/4405/gtfs_ota_20180129.zip'
-      log('Downloading GTFS Data from ORC')
-      const gtfsRequest = request({url: url}).pipe(fs.createWriteStream(otago.zipLocation))
-      gtfsRequest.on('finish', function() {
-        log('Finished Downloading GTFS Data')
-        resolve()
-      })
-      gtfsRequest.on('error', reject)
-    })
-  }
-}
+const otago = defaultConfig.get('otago', {
+  url: 'https://www.orc.govt.nz/transit/google_transit.zip',
+})
 module.exports = otago
