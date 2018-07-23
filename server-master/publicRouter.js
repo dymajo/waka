c7daeb82ed990b3cb404789053e234a2e942b3ad
Jono Cooper <jono@jonocooper.com>
2018-07-23 23:35:00 +1200

    Endpoint for Available Regions

--- server-master/publicRouter.js
+++ server-master/publicRouter.js
@@ -2,6 +2,7 @@ const httpProxy = require('http-proxy')
 const router = require('express').Router()
 const log = require('../server-common/logger.js')
 
+const cityMetadata = require('../cityMetadata.json')
 const Static = require('./static.js')
 const WorkerManager = require('./workerManager.js')
 
@@ -40,6 +41,52 @@ const staticServer = new Static()
 staticServer.start()
 router.all('/', staticServer.route)
 router.all('/a/email', staticServer.route)
+
+/**
+ * @api {get} /regions Get Available Regions
+ * @apiName GetRegions
+ * @apiGroup Info
+ *
+ * @apiSuccess {Object} region Object of available regions
+ * @apiSuccess {String} region.prefix Region Prefix
+ * @apiSuccess {String} region.name Name of the Region
+ * @apiSuccess {String} region.secondaryName Extra Region Name (State, Country etc)
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "nz-akl": {
+ *         "prefix": "nz-akl",
+ *         "name": "Tāmaki Makaurau",
+ *         "secondaryName": "Auckland"
+ *       },
+ *       "nz-syd": {
+ *         "prefix": "nz-chc",
+ *         "name": "Sydney",
+ *         "secondaryName": "New South Wales"
+ *       }
+ *     }
+ *
+ */
+router.get('/a/regions', (req, res) => {
+  const availableRegions = {}
+  const allMappings = WorkerManager.getAllMappings()
+  Object.keys(allMappings).forEach(region => {
+    const regionPair = allMappings[region].split('|')
+    if (
+      regionPair[0] !== 'null' &&
+      WorkerManager.getPort(regionPair[0], regionPair[1])
+    ) {
+      const meta = cityMetadata[region]
+      availableRegions[region] = {
+        prefix: region,
+        name: meta.name,
+        secondaryName: meta.secondaryName,
+      }
+    }
+  })
+  res.send(availableRegions)
+})
 router.all('/a/:prefix', proxyHandle)
 router.all('/a/:prefix/*', proxyHandle)
 router.all('/a/*', proxyHandle)
