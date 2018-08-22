fe94b8fc9618f199b2e92212cd4afe6463eb7913
mjdavidson <matt@mattdavidson.kiwi>
2018-08-22 18:22:10 +1000

    remove process.send

--- server/index.js
+++ server/index.js
@@ -5,84 +5,77 @@ const cache = require('./cache');
 
 log('Worker Started');
 
-(() => {
-  const {
-    ID,
-    PREFIX,
-    VERSION,
-    STATUS,
-    START_POLICY,
-    DB_CONFIG,
-    DB_NAME,
-    DB_USER,
-    DB_PASSWORD,
-    DB_SERVER,
-    DB_DATABASE,
-    DB_TRANSACTION_LIMIT,
-    DB_CONNECTION_TIMEOUT,
-    DB_REQUEST_TIMEOUT
-  } = process.env;
-  console.log(process.env.PREFIX);
+const {
+  ID,
+  PREFIX,
+  VERSION,
+  STATUS,
+  START_POLICY,
+  DB_CONFIG,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_SERVER,
+  DB_DATABASE,
+  DB_TRANSACTION_LIMIT,
+  DB_CONNECTION_TIMEOUT,
+  DB_REQUEST_TIMEOUT
+} = process.env;
+global.config = {
+  id: ID,
+  prefix: PREFIX,
+  version: VERSION,
+  status: STATUS,
+  startpolicy: START_POLICY,
+  dbconfig: DB_CONFIG,
+  dbname: DB_NAME,
+  db: {
+    user: DB_USER,
+    password: DB_PASSWORD,
+    server: DB_SERVER,
+    database: DB_DATABASE,
+    transactionLimit: DB_TRANSACTION_LIMIT,
+    connectionTimeout: DB_CONNECTION_TIMEOUT,
+    requestTimeout: DB_REQUEST_TIMEOUT
+  }
+};
 
-  global.config = {
-    id: ID,
-    prefix: PREFIX,
-    version: VERSION,
-    status: STATUS,
-    startpolicy: START_POLICY,
-    dbconfig: DB_CONFIG,
-    dbname: DB_NAME,
-    db: {
-      user: DB_USER,
-      password: DB_PASSWORD,
-      server: DB_SERVER,
-      database: DB_DATABASE,
-      transactionLimit: DB_TRANSACTION_LIMIT,
-      connectionTimeout: DB_CONNECTION_TIMEOUT,
-      requestTimeout: DB_REQUEST_TIMEOUT
-    }
-  };
+log(
+  'prefix: '.magenta,
+  global.config.prefix,
+  '\n       version:'.magenta,
+  global.config.version
+);
 
-  log(
-    'prefix: '.magenta,
-    global.config.prefix,
-    '\n       version:'.magenta,
-    global.config.version
-  );
+// connect to db
+connection.open().then(() => {
+  log('Connected to Database');
 
-  // connect to db
-  connection.open().then(() => {
-    log('Connected to Database');
-
-    const sqlRequest = connection.get().request();
-    sqlRequest
-      .query(
-        `
+  const sqlRequest = connection.get().request();
+  sqlRequest
+    .query(
+      `
         select OBJECT_ID('agency', 'U') as 'dbcreated'
       `
-      )
-      .then(data => {
-        console.log(data);
-        if (data.recordset[0].dbcreated === null) {
-          log('Building Database from Template');
-          const creator = new createDb();
-          creator
-            .start()
-            .then(() => {
-              log('Worker Ready');
-              process.send({ type: 'ready' });
-            })
-            .catch(err => {
-              console.log(err);
-            });
-        } else {
-          log('Worker Ready');
-          cache.runReady();
-          process.send({ type: 'ready' });
-        }
-      });
-  });
-})();
+    )
+    .then(data => {
+      if (data.recordset[0].dbcreated === null) {
+        log('Building Database from Template');
+        const creator = new createDb();
+        creator
+          .start()
+          .then(() => {
+            log('Worker Ready');
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        log('Worker Ready');
+        cache.runReady();
+      }
+    });
+});
 
 let lastbeat = new Date();
 
