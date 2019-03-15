ddde1a70afa7361e6ea8b1e7a0b63330468e41f4
Matt Davidson <matt@mattdavidson.kiwi>
2019-03-15 15:58:05 +1100

    start realtime updater for au-syd

--- waka-worker/realtime/regions/au-syd.js
+++ waka-worker/realtime/regions/au-syd.js
@@ -1,130 +1,114 @@
-// TODO: Busted, needs to be a class, and probably needs to be rewritten anyway.
-const request = require('request')
-const path = require('path')
-const protobuf = require('protobufjs')
+const GtfsRealtimeBindings = require('gtfs-realtime-bindings')
+const fetch = require('node-fetch')
 
-const log = require('../../server-common/logger.js')
-const search = require('../stops/search.js')
+const schedulePullTimeout = 20000
+const scheduleLocationPullTimeout = 15000
 
-const routeTypeMap = new Map()
-routeTypeMap.set(0, 'lightrail')
-routeTypeMap.set(2, 'sydneytrains')
-routeTypeMap.set(3, 'buses')
-routeTypeMap.set(4, 'ferries')
+const modes = [
+  'buses',
+  'ferries',
+  'lightrail/innerwest',
+  'lightrail/newcastle',
+  'nswtrains',
+  'sydneytrains',
+]
 
-const modeTypeMap = new Map()
-routeTypeMap.forEach((value, key) => {
-  modeTypeMap.set(value, key)
-})
+class RealtimeAUSYD {
+  constructor(props) {
+    const { apiKey, connection, logger } = props
+    this.connection = connection
+    this.logger = logger
+    this.apiKey = apiKey
 
-const routeMapper = {
-  route: mode => routeTypeMap.get(mode),
-  mode: route => modeTypeMap.get(route),
-}
+    this.lastUpdate = null
+    this.lastVehicleUpdate = null
+    this.currentData = {}
+    this.currentDataFails = 0
+    this.currentVehicleData = {}
+    this.currentVehicleDataFails = null
 
-const modes = ['sydneytrains', 'buses', 'ferries', 'lightrail', 'nswtrains']
-
-const defaultDownloadOptions = JSON.stringify({
-  url: 'https://api.transport.nsw.gov.au/v1/gtfs/realtime/',
-  encoding: null,
-  headers: {
-    Authorization: process.env.nswApiKey,
-    Accept: 'application/x-protobuf',
-  },
-})
-
-const wait = async function(ms) {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, ms)
-  })
-}
-const realtime = {
-  currentData: {},
-  currentDataFails: 0,
-  schedulePull: async function() {
-    for (let mode of modes) {
-      const newOptions = JSON.parse(defaultDownloadOptions)
-      newOptions.url += mode
-      try {
-        realtime.currentData[mode] = await realtime.tripUpdate(newOptions, mode)
-        // console.log('got', mode)
-      } catch (err) {
-        console.log(err)
-      }
-      await wait(250)
+    this.tripUpdateOptions = {
+      url: 'https://api.transport.nsw.gov.au/v1/gtfs/realtime',
+      headers: {
+        Authorization: apiKey,
+      },
     }
-    setTimeout(realtime.schedulePull, 20000)
-  },
-  getTripsEndpoint: function(req, res) {
-    const rt = realtime.getTripsCachedSydney(req.body.stop_id, req.body.trips)
-    res.send(rt)
-  },
-  getVehicleLocationEndpoint: function() {},
-  getTripsCachedSydney: function(stop_id, trips) {
-    const realtimeInfo = {}
-    Object.keys(trips).forEach(function(trip) {
-      const routeType = search.stopsRouteType[stop_id]
-      const mode = routeMapper.route(routeType)
-      // console.log('mode', mode)
-      const data = realtime.currentData[mode][trip]
-      // console.log(realtime.currentData[mode])
-      // console.log(data)
-      if (typeof data !== 'undefined') {
-        // console.log('it defined')
-        const timeUpdate =
-          data.stopTimeUpdate.departure || data.stopTimeUpdate.arrival || {}
-        realtimeInfo[trip] = {
-          stop_sequence: data.stopTimeUpdate.stopSequence,
-          delay: timeUpdate.delay,
-          timeStamp: timeUpdate.time,
-          v_id: data.vehicle.id,
-        }
-      }
-    })
-    return realtimeInfo
-  },
-  initPB: function() {
-    protobuf.load(path.resolve(__dirname, './gtfs-realtime.proto'), function(
-      err,
-      root
-    ) {
-      if (err) {
-        throw err
-      }
-      log('Protobuf Loaded')
-      realtime.decoder = root
-    })
-  },
-  tripUpdate: function(newOptions, mode) {
-    return new Promise(function(resolve, reject) {
-      request(newOptions, function(error, response, body) {
-        if (error) {
-          realtime.currentDataFails++
-          return reject(error)
-        }
-        try {
-          const gtfs = realtime.decoder.lookupType('FeedMessage')
-          const message = gtfs.decode(body)
-          if (message) {
-            // console.log(message.entity.length, mode)
-            const newData = {}
-            message.entity.forEach(function(trip) {
-              newData[trip.id] = trip.tripUpdate
-            })
-            realtime.currentDataFails = 0
-            resolve(newData)
-          } else {
-            log('could not get any data from tfnsw')
-          }
-          resolve()
-        } catch (err) {
-          realtime.currentDataFails++
-          return reject(err)
-        }
+
+    this.vehicleLocationOptions = {
+      url: 'https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos',
+      headers: {
+        Authorization: apiKey,
+      },
+    }
+
+    this.schedulePull = this.schedulePull.bind(this)
+    this.scheduleLocationPull = this.scheduleLocationPull.bind(this)
+  }
+
+  start() {
+    const { apiKey, logger } = this
+    if (!apiKey) {
+      logger.warn('No TfNSW API Key, will not show realtime.')
+    }
+    this.schedulePull()
+    // this.scheduleLocationPull()
+    logger.info('TfNSW Realtime Started.')
+  }
+
+  async schedulePull() {
+    const { logger, tripUpdateOptions } = this
+    const newData = {}
+    modes.forEach(async mode => {
+      const data = await fetch(`${tripUpdateOptions.url}/${mode}`, {
+        headers: tripUpdateOptions.headers,
       })
+      if (data.ok) {
+        const body = await data.text()
+        const buffer = Buffer.from(body)
+        try {
+          const feed = GtfsRealtimeBindings.FeedMessage.decode(buffer)
+          feed.entity.forEach(trip => {
+            console.log(trip)
+            if (trip.trip_update) {
+              newData[trip.trip_update.trip.trip_id] = trip.trip_update
+            }
+          })
+        } catch (error) {
+          console.error(error)
+        }
+      } else {
+        console.log(data.statusText)
+      }
     })
-  },
+    this.currentData = newData
+    this.currentDataFails = 0
+    this.lastUpdate = new Date()
+    setTimeout(this.schedulePull, schedulePullTimeout)
+  }
+
+  async scheduleLocationPull() {
+    const { logger, vehicleLocationOptions } = this
+    const newVehicleData = {}
+    modes.forEach(async mode => {
+      const data = await fetch(`${vehicleLocationOptions.url}/${mode}`, {
+        headers: vehicleLocationOptions.headers,
+      })
+      if (data.ok) {
+        const body = await data.text()
+        const buffer = Buffer.from(body)
+
+        const feed = GtfsRealtimeBindings.FeedMessage.decode(buffer)
+        feed.entity.forEach(trip => {
+          console.log(trip)
+          if (trip.trip_update) {
+            newVehicleData[trip.trip_update.trip.trip_id] = trip.trip_update
+          }
+        })
+      } else {
+        console.log(data.statusText)
+      }
+    })
+  }
 }
 
-realtime.initPB()
-module.exports = realtime
+module.exports = RealtimeAUSYD
