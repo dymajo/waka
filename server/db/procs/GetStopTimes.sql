909771b12f6376810568fd95228778460f9aa10d
Matt Davidson <matt@mattdavidson.kiwi>
2019-03-18 17:03:13 +1100

    update get stops times

--- server/db/procs/GetStopTimes.sql
+++ server/db/procs/GetStopTimes.sql
@@ -1,6 +1,7 @@
 -- =============================================
--- Author:		Jono Cooper <jono@jonocooper.com>
+-- Author:		Jono Cooper <jono@jonocooper.com> and Matt Davidson <matt@mattdavidson.kiwi>
 -- Create date: 2017-07-30
+-- Updated: 2019-03-18
 -- Description:	Retrieves stop times for a station.
 -- =============================================
 CREATE PROCEDURE [dbo].[GetStopTimes]
@@ -9,7 +10,9 @@ CREATE PROCEDURE [dbo].[GetStopTimes]
 	@date date
 AS
 BEGIN
-	SET NOCOUNT ON;
+SET NOCOUNT ON;
+
+
 	DECLARE @DepatureDelay INT = -30;
 	DECLARE @DepatureFuture INT = 180;
 	DECLARE @Day INT = DATEPART(dw, @date);
@@ -21,6 +24,7 @@ BEGIN
 		stop_times.stop_sequence,
 		stop_times.departure_time,
 		stop_times.departure_time_24,
+		stop_times.stop_id,
 		trips.trip_headsign,
 		trips.shape_id,
 		trips.direction_id,
@@ -30,7 +34,8 @@ BEGIN
 		routes.route_long_name,
 		routes.route_type,
 		routes.agency_id,
-		routes.route_color
+		routes.route_color,
+		stops.stop_name
 	FROM stop_times
 	LEFT JOIN stops
 		on stop_times.stop_id = stops.stop_id
@@ -44,7 +49,8 @@ BEGIN
 		on trips.service_id = calendar_dates.service_id and
 		calendar_dates.date = @date
 	WHERE
-		stops.stop_code = @stop_id and
+		(trip_headsign <> 'Empty Train' or trip_headsign <> 'Out Of Service') and
+		(stops.parent_station = @stop_id or stops.stop_code = @stop_id) and
 		(
 			departure_time > DATEADD(MINUTE, @DepatureDelay, @departure_time) and
 			departure_time < CASE WHEN @DateDifference > 0 THEN DATEADD(MINUTE, @DepatureFuture, @departure_time) ELSE '23:59:59' END or
@@ -53,7 +59,7 @@ BEGIN
 		and (exception_type is null or exception_type != 2) and
 		@date >= calendar.start_date and
 		@date <= calendar.end_date and
-		(CASE 
+		(CASE
 			WHEN @Day = 1 THEN calendar.sunday
             WHEN @Day = 2 THEN calendar.monday
             WHEN @Day = 3 THEN calendar.tuesday
@@ -61,7 +67,7 @@ BEGIN
 			WHEN @Day = 5 THEN calendar.thursday
 			WHEN @Day = 6 THEN calendar.friday
 			WHEN @Day = 7 THEN calendar.saturday
-            ELSE 0 
+            ELSE 0
         END = 1 or exception_type = 1)
 
 	ORDER BY departure_time asc
