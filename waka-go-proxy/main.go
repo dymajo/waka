b63024f8d3513cfaa6ed2e9c611664ed0367d905
Jono Cooper <jono@jonocooper.com>
2019-05-12 01:42:32 +1200

    Serve API Doc

--- waka-go-proxy/main.go
+++ waka-go-proxy/main.go
@@ -15,6 +15,7 @@ var (
 	path       string
 	interval   int
 	pathPrefix string
+	docsDir		 string
 )
 
 func init() {
@@ -23,8 +24,15 @@ func init() {
 	flag.StringVar(&path, "f", "../cityMetadata.json", "path to city metadata json")
 	flag.IntVar(&interval, "m", 1, "minutes between requests")
 	flag.StringVar(&pathPrefix, "pathprefix", "/", "you can prefix all the routes, if you need")
+	flag.StringVar(&docsDir, "docsdir", "../dist/docs", "the path to the documentatino directory")
 	flag.Parse()
 	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
+func docsRedirectHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/docs/", http.StatusFound)
+	}
 }
 
 func main() {
@@ -38,6 +46,8 @@ func main() {
 	subRouter := router.PathPrefix(pathPrefix).Subrouter()
 	subRouter.HandleFunc("/ping", ping.Handler()).Methods("GET", "HEAD")
 	subRouter.HandleFunc("/regions", workerDiscovery.RegionsHandler()).Methods("GET", "HEAD")
+	subRouter.HandleFunc("/docs", docsRedirectHandler())
+	subRouter.PathPrefix("/docs/").Handler(http.StripPrefix("/docs", http.FileServer(http.Dir(docsDir))))
 	subRouter.PathPrefix("/{prefix}").HandlerFunc(workerDiscovery.BoundsHandler(pathPrefix)).Methods("GET", "HEAD")
 
 	server := &http.Server{
