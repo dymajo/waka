d9b94873ebb3668e29fa2e6c76c51fd61e7eef84
Matt Davidson <matt@mattdavidson.kiwi>
2019-07-23 16:25:00 +1000

    show story for 12/24h tripitem in storybook

--- stories/tripitem.stories.jsx
+++ stories/tripitem.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import { TripItem } from '../js/views/station/TripItemV2.jsx'
+import SettingsStore from '../js/stores/SettingsStore.js'
 
 storiesOf('TripItem', module)
   .add('single trip', () => (
@@ -82,19 +83,44 @@ storiesOf('TripItem', module)
       ]}
     />
   ))
-  .add('3 hours away', () => (
-    <TripItem
-      routeShortName="70"
-      direction={0}
-      trips={[
-        {
-          destination: 'Botany',
-          departureTime: new Date(new Date().getTime() + 1000 * 60 * 180),
-          isRealtime: false,
-        },
-      ]}
-    />
-  ))
+  .add('3 hours away 24h', () => {
+    SettingsStore.state.clock = false
+    SettingsStore.saveState()
+    return (
+      <>
+        <TripItem
+          routeShortName="70"
+          direction={0}
+          trips={[
+            {
+              destination: 'Botany',
+              departureTime: new Date(new Date().getTime() + 1000 * 60 * 180),
+              isRealtime: false,
+            },
+          ]}
+        />
+      </>
+    )
+  })
+  .add('3 hours away 12h', () => {
+    SettingsStore.state.clock = true
+    SettingsStore.saveState()
+    return (
+      <>
+        <TripItem
+          routeShortName="70"
+          direction={0}
+          trips={[
+            {
+              destination: 'Botany',
+              departureTime: new Date(new Date().getTime() + 1000 * 60 * 180),
+              isRealtime: false,
+            },
+          ]}
+        />
+      </>
+    )
+  })
   .add('two trips', () => (
     <TripItem
       routeShortName="70"
