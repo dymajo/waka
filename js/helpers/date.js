fc2fbd5b6af2a51e12a70b33f8e3f63f98959f64
Jono Cooper <jono@jonocooper.com>
2019-08-07 00:23:42 +1200

    Add Timetable to New Route Page

--- js/helpers/date.js
+++ js/helpers/date.js
@@ -1,16 +1,41 @@
-export const prefixToTimezone = prefix => {
-  switch (prefix) {
-  case 'au-syd':
-    return 'Australia/Sydney'
-  case 'au-mel':
-    return 'Australia/Melbourne'
-  case 'au-per':
-    return 'Australia/Perth'
-  case 'us-nyc':
-    return 'America/New_York'
-  case 'nz-wlg':
-  case 'nz-akl':
-  default:
-    return 'Pacific/Auckland'
+export const getTime = (date, isTwentyFourHour, showDue) => {
+  const now = new Date()
+  if (date <= now) {
+    if (showDue === true) {
+      return {
+        text: 'Due',
+      }
+    }
+    return {
+      minutes: '0', // hack - using a string is != false
+    }
+  }
+  const minutes = Math.ceil((date.getTime() - now.getTime()) / 60000)
+  if (minutes > 90) {
+    // be careful using 'default' as it will use browser default time formatting
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString#Using_locales
+    const dateText = date.toLocaleTimeString('default', {
+      // get 24h time from settings
+      hour12: !isTwentyFourHour,
+      hour: 'numeric',
+      minute: 'numeric',
+    })
+    // if us -> AM/PM, if au/nz -> am/pm, if gb -> 24h
+    return {
+      text: dateText.replace(/ (AM|PM|am|pm)/, ''),
+      subtext: dateText.match(/ (AM|PM|am|pm)/)
+        ? dateText.match(/ (AM|PM|am|pm)/)[0].toLowerCase()
+        : null,
+    }
+  }
+  // This enables hours, but we don't want that
+  // if (minutes >= 60) {
+  //   return {
+  //     hours: Math.floor(minutes / 60),
+  //     minutes: minutes % 60,
+  //   }
+  // }
+  return {
+    minutes,
   }
 }
