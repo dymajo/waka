de44565faecbd4d1a8820f4b75d281d13b3800c4
Jono Cooper <jono@jonocooper.com>
2019-08-16 00:03:41 +1200

    Add Realtime Support to Timetable Component

--- js/views/lines/Timetable.jsx
+++ js/views/lines/Timetable.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
@@ -6,8 +6,9 @@ import { vars } from '../../styles.js'
 
 import { getTime } from '../../helpers/date.js'
 
-const formatDate = dateString => {
+const formatDate = (dateString, delay) => {
   const date = new Date(dateString)
+  date.setTime(date.getTime() + delay * 1000)
   const humanTime = getTime(date, false, true)
 
   // make this nicer
@@ -18,29 +19,93 @@ const formatDate = dateString => {
 
 let styles = null
 
-const Timetable = ({ timetable, currentTrip, triggerTrip }) => {
+const Timetable = ({
+  timetable,
+  currentTrip,
+  triggerTrip,
+  realtimeStopUpdates,
+}) => {
   return (
     <View>
       <Text style={styles.direction}>Departures</Text>
       <View style={styles.departures}>
         {timetable
-          .filter(service => service.visible === true)
-          .map(service => (
-            <TouchableOpacity
-              key={[service.trip_id, service.departure_time].join()}
-              style={
-                currentTrip === service.trip_id
-                  ? [styles.departure, styles.departureSelected]
-                  : styles.departure
+          .map(service => {
+            const realtimeTrip = realtimeStopUpdates[service.trip_id]
+            if (realtimeTrip && realtimeTrip.stopTimeUpdate.length > 0) {
+              let stop = realtimeTrip.stopTimeUpdate.find(
+                i => i.stopSequence === service.stop_sequence
+              )
+              if (!stop) [stop] = realtimeTrip.stopTimeUpdate
+              service.realtimeStop = stop
+
+              if (stop.stopSequence) {
+                service.visible = stop.stopSequence <= service.stopSequence
               }
-              onPress={triggerTrip(service.trip_id)}
-            >
-              <Text style={styles.departureDate}>
-                {formatDate(service.departure_time)}
-              </Text>
-              <Text style={styles.departureStatus}>Scheduled</Text>
-            </TouchableOpacity>
-          ))}
+            }
+            return service
+          })
+          .filter(service => service.visible === true)
+          .map(service => {
+            let departureTextStyle = null
+            let emotion = null
+            let delay = 0
+            let scheduleRelationship = 'scheduled'
+
+            if (service.realtimeStop) {
+              const stop = service.realtimeStop
+              scheduleRelationship = stop.scheduleRelationship.toLowerCase()
+
+              if (stop.scheduleRelationship === 'SCHEDULED') {
+                const estimate = stop.departure || stop.arrival
+                if (estimate.time) {
+                  delay =
+                    (new Date(
+                      Math.round(parseInt(estimate.time, 10) * 1000)
+                    ).getTime() -
+                      new Date(
+                        service.departure_time || service.arrival_time
+                      ).getTime()) /
+                    1000
+                } else {
+                  delay = estimate.delay
+                }
+
+                const delayText = `(${Math.ceil(Math.abs(delay) / 60)}m)`
+                if (delay < -90) {
+                  emotion = styles.neutral
+                  scheduleRelationship = <Fragment>early {delayText}</Fragment>
+                } else if (delay < 180) {
+                  emotion = styles.positive
+                  scheduleRelationship = 'on time'
+                } else if (delay >= 180) {
+                  emotion = styles.negative
+                  scheduleRelationship = <Fragment>late {delayText}</Fragment>
+                }
+              } else if (stop.scheduleRelationship === 'CANCELLED') {
+                departureTextStyle = styles.cancelled
+                emotion = styles.negative
+              }
+            }
+            return (
+              <TouchableOpacity
+                key={[service.trip_id, service.departure_time].join()}
+                style={
+                  currentTrip === service.trip_id
+                    ? [styles.departure, styles.departureSelected]
+                    : styles.departure
+                }
+                onPress={triggerTrip(service.trip_id)}
+              >
+                <Text style={[styles.departureDate, departureTextStyle]}>
+                  {formatDate(service.departure_time, delay)}
+                </Text>
+                <Text style={[styles.departureStatus, emotion]}>
+                  {scheduleRelationship}
+                </Text>
+              </TouchableOpacity>
+            )
+          })}
       </View>
     </View>
   )
@@ -85,11 +150,25 @@ styles = StyleSheet.create({
     fontFamily: vars.fontFamily,
     fontSize: 16,
   },
+  cancelled: {
+    textDecorationStyle: 'solid',
+    textDecorationLine: 'line-through',
+  },
   departureStatus: {
     textAlign: 'center',
     fontFamily: vars.fontFamily,
     fontSize: 13,
     color: '#888',
+    textTransform: 'capitalize',
+  },
+  positive: {
+    color: '#27ae60',
+  },
+  neutral: {
+    color: '#2980b9',
+  },
+  negative: {
+    color: '#c0392b',
   },
   direction: {
     paddingTop: vars.padding,
--- stories/timetable.stories.jsx
+++ stories/timetable.stories.jsx
@@ -2,13 +2,259 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Timetable from '../js/views/lines/Timetable'
 
-storiesOf('timetable', module).add('2 trips', () => (
-  <Timetable
-    currentTrip="1"
-    timetable={[
-      { trip_id: '1', departure_time: 1 },
-      { trip_id: '2', departure_time: 2000 },
-    ]}
-    triggerTrip={() => {}}
-  />
-))
+const now = new Date().toISOString()
+const tenMins = new Date(new Date(now).getTime() + 10 * 60000).toISOString()
+const hour = new Date(new Date(now).getTime() + 60 * 60000).toISOString()
+const past = new Date(new Date(now).getTime() - 10 * 60000).toISOString()
+
+storiesOf('Timetable', module)
+  .add('1 trip', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[{ trip_id: '1', departure_time: now, visible: true }]}
+      realtimeStopUpdates={{}}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('2 trips', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+      ]}
+      realtimeStopUpdates={{}}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('3 trips', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{}}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('selected trip', () => (
+    <Timetable
+      currentTrip="2"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{}}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - cancelled', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '1': { stopTimeUpdate: [{ scheduleRelationship: 'CANCELLED' }] },
+        '2': { stopTimeUpdate: [{ scheduleRelationship: 'CANCELLED' }] },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - time prop', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '1': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: 0,
+                time: Math.round(
+                  new Date(now).getTime() / 1000 + 290
+                ).toString(),
+              },
+            },
+          ],
+        },
+        '2': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: 0,
+                time: Math.round(
+                  new Date(tenMins).getTime() / 1000 + 290
+                ).toString(),
+              },
+            },
+          ],
+        },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - late', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '1': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: 290,
+              },
+            },
+          ],
+        },
+        '2': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: 290,
+              },
+            },
+          ],
+        },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - on time', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '1': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: -60,
+              },
+            },
+          ],
+        },
+        '2': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: 60,
+              },
+            },
+          ],
+        },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - early', () => (
+    <Timetable
+      currentTrip="1"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '1': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: -600,
+              },
+            },
+          ],
+        },
+        '2': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              departure: {
+                delay: -300,
+              },
+            },
+          ],
+        },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - very late', () => (
+    <Timetable
+      currentTrip="0"
+      timetable={[
+        {
+          trip_id: '0',
+          departure_time: past,
+          visible: false,
+          stopSequence: 15,
+        },
+        { trip_id: '1', departure_time: now, visible: true },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '0': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              stopSequence: 10,
+              departure: {
+                delay: 900,
+              },
+            },
+          ],
+        },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
+  .add('realtime - very early', () => (
+    <Timetable
+      currentTrip="2"
+      timetable={[
+        { trip_id: '1', departure_time: now, visible: true, stopSequence: 15 },
+        { trip_id: '2', departure_time: tenMins, visible: true },
+        { trip_id: '3', departure_time: hour, visible: true },
+      ]}
+      realtimeStopUpdates={{
+        '1': {
+          stopTimeUpdate: [
+            {
+              scheduleRelationship: 'SCHEDULED',
+              stopSequence: 20,
+              departure: {
+                delay: -300,
+              },
+            },
+          ],
+        },
+      }}
+      triggerTrip={() => {}}
+    />
+  ))
