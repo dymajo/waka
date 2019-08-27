ace2a2bd99a5840c02a92c83cee1064ad63377f4
Matt Davidson <matt@mattdavidson.kiwi>
2019-08-27 11:35:36 +1000

    add route text color to get stop times

--- server/db/procs/GetStopTimes.sql
+++ server/db/procs/GetStopTimes.sql
@@ -21,68 +21,69 @@ BEGIN
 
 	SELECT
 		CASE
-			WHEN stop_time.arrival_time_24 = 1 THEN DATEDIFF(s, cast('00:00' AS TIME), stop_time.arrival_time) + 86400
-			ELSE DATEDIFF(s, cast('00:00' AS TIME), stop_time.arrival_time)
+			WHEN st.arrival_time_24 = 1 THEN DATEDIFF(s, cast('00:00' AS TIME), st.arrival_time) + 86400
+			ELSE DATEDIFF(s, cast('00:00' AS TIME), st.arrival_time)
 		END AS new_arrival_time,
 		CASE
-			WHEN stop_time.departure_time_24 = 1 THEN DATEDIFF(s, cast('00:00' AS TIME), stop_time.departure_time) + 86400
-			ELSE DATEDIFF(s, cast('00:00' AS TIME), stop_time.departure_time)
+			WHEN st.departure_time_24 = 1 THEN DATEDIFF(s, cast('00:00' AS TIME), st.departure_time) + 86400
+			ELSE DATEDIFF(s, cast('00:00' AS TIME), st.departure_time)
 		END AS new_departure_time,
-		stop_time.trip_id,
-		stop_time.stop_sequence,
-		stop_time.arrival_time,
-		stop_time.arrival_time_24,
-		stop_time.departure_time,
-		stop_time.departure_time_24,
-		stop_time.stop_id,
-		stop_time.pickup_type,
-		stop_time.drop_off_type,
-		trip.trip_headsign,
-		trip.shape_id,
-		trip.direction_id,
-		calendar.start_date,
-		calendar.end_date,
-		route.route_short_name,
-		route.route_long_name,
-		route.route_type,
-		route.route_id,
-		route.agency_id,
-		route.route_color,
-		stop.stop_name
-	FROM [dbo].[stop_times] stop_time
-		LEFT JOIN [dbo].[stops] stop
-		ON stop_time.stop_id = stop.stop_id
-		LEFT JOIN [dbo].[trips] trip
-		ON stop_time.trip_id = trip.trip_id
-		LEFT JOIN [dbo].[routes] route
-		ON trip.route_id = route.route_id
-		LEFT JOIN [dbo].[calendar] calendar
-		ON trip.service_id = calendar.service_id
-		LEFT JOIN [dbo].[calendar_dates] calendar_date
-		ON trip.service_id = calendar_date.service_id AND
-			calendar_date.date = @departure_date
+		st.trip_id,
+		st.stop_sequence,
+		st.arrival_time,
+		st.arrival_time_24,
+		st.departure_time,
+		st.departure_time_24,
+		st.stop_id,
+		st.pickup_type,
+		st.drop_off_type,
+		t.trip_headsign,
+		t.shape_id,
+		t.direction_id,
+		c.start_date,
+		c.end_date,
+		r.route_short_name,
+		r.route_long_name,
+		r.route_type,
+		r.route_id,
+		r.agency_id,
+		r.route_color,
+		r.route_text_color,
+		s.stop_name
+	FROM [dbo].[stop_times] st
+		LEFT JOIN [dbo].[stops] s
+		ON st.stop_id = s.stop_id
+		LEFT JOIN [dbo].[trips] t
+		ON st.trip_id = t.trip_id
+		LEFT JOIN [dbo].[routes] r
+		ON t.route_id = r.route_id
+		LEFT JOIN [dbo].[calendar] c
+		ON t.service_id = c.service_id
+		LEFT JOIN [dbo].[calendar_dates] cd
+		ON t.service_id = cd.service_id AND
+			cd.date = @departure_date
 	WHERE
-		trip.trip_headsign <> 'Empty Train'
-		AND trip.trip_headsign <> 'Out Of Service'
-		AND (stop.parent_station = @stop_id OR stop.stop_code = @stop_id)
-		AND stop_time.pickup_type = 0
+		t.trip_headsign <> 'Empty Train'
+		AND t.trip_headsign <> 'Out Of Service'
+		AND (s.parent_station = @stop_id OR s.stop_code = @stop_id)
+		AND st.pickup_type = 0
 		AND (
-			stop_time.departure_time > DATEADD(MINUTE, @DepatureDelay, @departure_time) AND
-		stop_time.departure_time < CASE WHEN @DateDifference > 0 THEN DATEADD(MINUTE, @DepatureFuture, @departure_time) ELSE '23:59:59' END OR
-		stop_time.departure_time < CASE WHEN @DateDifference <= 0 THEN DATEADD(MINUTE, @DepatureFuture, @departure_time) ELSE '00:00:00' END
+			st.departure_time > DATEADD(MINUTE, @DepatureDelay, @departure_time) AND
+		st.departure_time < CASE WHEN @DateDifference > 0 THEN DATEADD(MINUTE, @DepatureFuture, @departure_time) ELSE '23:59:59' END OR
+		st.departure_time < CASE WHEN @DateDifference <= 0 THEN DATEADD(MINUTE, @DepatureFuture, @departure_time) ELSE '00:00:00' END
 		)
-		AND (calendar_date.exception_type IS NULL OR calendar_date.exception_type <> 2) AND
-		@departure_date >= calendar.start_date AND
-		@departure_date <= calendar.end_date AND
+		AND (cd.exception_type IS NULL OR cd.exception_type <> 2) AND
+		@departure_date >= c.start_date AND
+		@departure_date <= c.end_date AND
 		(CASE
-			WHEN @Day = 1 THEN calendar.sunday
-            WHEN @Day = 2 THEN calendar.monday
-            WHEN @Day = 3 THEN calendar.tuesday
-			WHEN @Day = 4 THEN calendar.wednesday
-			WHEN @Day = 5 THEN calendar.thursday
-			WHEN @Day = 6 THEN calendar.friday
-			WHEN @Day = 7 THEN calendar.saturday
+			WHEN @Day = 1 THEN c.sunday
+            WHEN @Day = 2 THEN c.monday
+            WHEN @Day = 3 THEN c.tuesday
+			WHEN @Day = 4 THEN c.wednesday
+			WHEN @Day = 5 THEN c.thursday
+			WHEN @Day = 6 THEN c.friday
+			WHEN @Day = 7 THEN c.saturday
             ELSE 0
-        END = 1 OR calendar_date.exception_type = 1)
+        END = 1 OR cd.exception_type = 1)
 	ORDER BY new_departure_time ASC;
 END;
