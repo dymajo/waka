1da6a5dc9122f7ae61f1abdf0f26ef515574d4be
Jono Cooper <jono@jonocooper.com>
2019-08-31 22:50:32 +1200

    Move Wellington Colors into Importer

--- server/importers/regions/nz-wlg.ts
+++ server/importers/regions/nz-wlg.ts
@@ -20,12 +20,19 @@ class WellingtonImporter extends SingleImporter {
       UPDATE trips
       SET trips.trip_headsign = stop_times.stop_headsign
       FROM trips JOIN stop_times ON trips.trip_id = stop_times.trip_id
-      WHERE stop_sequence = 0 and trips.trip_headsign is null
+      WHERE stop_sequence = 0 and trips.trip_headsign is null;
     `)
-    log.info(
-      `${config.prefix} ${config.version}`,
-      'Post Import: Completed Trip Headsign Override',
-    )
+    log.info('Post Import: Completed Trip Headsign Override')
+
+    await sqlRequest.query("UPDATE routes SET route_color = '333333' WHERE route_color = '000000'")
+    await sqlRequest.query("UPDATE routes SET route_color = '41bada' WHERE agency_id = 'EBYW'")
+    await sqlRequest.query("UPDATE routes SET route_color = 'e52f2b' WHERE route_short_name = 'HVL'")
+    await sqlRequest.query("UPDATE routes SET route_color = '4f9734' WHERE route_short_name = 'JVL'")
+    await sqlRequest.query("UPDATE routes SET route_color = 'f39c12' WHERE route_short_name = 'KPL'")
+    await sqlRequest.query("UPDATE routes SET route_color = '21b4e3' WHERE route_short_name = 'MEL'")
+    await sqlRequest.query("UPDATE routes SET route_color = 'e52f2b' WHERE route_short_name = 'WRL'")
+    await sqlRequest.query("UPDATE routes SET route_text_color = 'ffffff'")
+    log.info('Post Import: Updated Colors')
   }
 }
 
