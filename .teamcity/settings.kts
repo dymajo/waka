73255b1d51e203cc683dc708424acb6267931d22
Jono Cooper <jono@jonocooper.com>
2019-09-17 22:10:14 +1200

    Fix Typo

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -38,7 +38,8 @@ object Build : BuildType({
         script {
             name = "Install AWS CLI"
             scriptContent = """
-                apt-update && apt install -y unzip  
+                apt update
+                apt install -y unzip  
                 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
                 unzip awscli-bundle.zip
                 ./awscli-bundle/install -b ~/bin/aws
