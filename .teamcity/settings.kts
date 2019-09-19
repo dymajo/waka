e161fbd5c312a282824472b104534395751f62e4
Jono Cooper <jono@jonocooper.com>
2019-09-19 22:28:51 +1200

    Add Commit Status to TC

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
@@ -49,6 +52,25 @@ object Build : BuildType({
     triggers {
         vcs {
             branchFilter = "+:*"
+        }
+    }
+    
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = DslContext.settingsRoot.id!!.value
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "%github.commit_status_publisher_token%"
+                }
+            }
+        }
+        pullRequests {
+            provider = github {
+                authType = vcsRoot()
+                // Only run pull requests for authors who are members or colloborators
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER_OR_COLLABORATOR
+            }
         }
     }
 })
