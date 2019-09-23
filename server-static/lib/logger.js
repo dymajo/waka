7b879f3dbe096d1dcf1161a2a0c9ba6548d771b8
Jono Cooper <jono@jonocooper.com>
2019-09-24 00:43:52 +1200

    Remove old server

--- server-static/lib/logger.js
+++
@@ -1,9 +0,0 @@
-let pid = `      ${process.pid.toString(16)}`.slice(-6)
-if (process.send === undefined) {
-  pid = 'master'
-}
-const log = function() {
-  const args = Array.from(arguments)
-  console.log(pid, ...args)
-}
-module.exports = log
