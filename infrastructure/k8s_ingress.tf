a876bdb9808c339b99e5a10b5cb2a49ccaee430f
Jono Cooper <jono@jonocooper.com>
2019-12-18 21:12:16 +1300

    Explicitly Define Host

--- infrastructure/k8s_ingress.tf
+++ infrastructure/k8s_ingress.tf
@@ -8,9 +8,18 @@ resource "kubernetes_ingress" "waka" {
   }
 
   spec {
-    backend {
-      service_name = kubernetes_service.waka.metadata.0.name
-      service_port = kubernetes_service.waka.spec.0.port.0.port
+    rule {
+      host = var.host_header
+      http {
+        path {
+          backend {
+            service_name = kubernetes_service.waka.metadata.0.name
+            service_port = kubernetes_service.waka.spec.0.port.0.port
+          }
+
+          path = "/"
+        }
+      }
     }
   }
 }
