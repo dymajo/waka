5a6c971050c3408311b326e3d4538d825534feaa
Jono Cooper <jono@jonocooper.com>
2020-01-04 00:19:26 +1300

    Remove ALB Config

--- infrastructure/lb.tf
+++ infrastructure/lb.tf
@@ -9,26 +9,3 @@ resource "aws_alb_target_group" "api_service_tg" {
     path = "/ping"
   }
 }
-
-data "aws_lb" "alb" {
-  name = "${var.load_balancer_name}"
-}
-
-data "aws_lb_listener" "https_listener" {
-  load_balancer_arn = "${data.aws_lb.alb.arn}"
-  port = 443
-}
-
-resource "aws_alb_listener_rule" "api_service_https_listener" {
-  listener_arn = data.aws_lb_listener.https_listener.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.api_service_tg.id
-  }
-
-  condition {
-    field  = "host-header"
-    values = ["${var.host_header}"]
-  }
-}
