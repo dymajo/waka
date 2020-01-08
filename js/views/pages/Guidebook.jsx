5e05467c3ca02e82b6d1b71721fda3face0be2f6
Jono Cooper <jono@jonocooper.com>
2020-01-08 22:59:47 +1300

    Add Guidebook View

---
+++ js/views/pages/Guidebook.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { useLocation } from 'react-router-dom'
+
+import Header from '../reusable/Header.jsx'
+import LinkedScroll from '../reusable/LinkedScroll.jsx'
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+})
+
+const endpoint = 'https://dymajo.com/waka-guidebooks/regions'
+const pageCache = {}
+
+const fetchPage = async location => {
+  const url = `${endpoint}/${location.substring('/guide/'.length)}`
+
+  if (pageCache[url] === undefined) {
+    const res = await fetch(url, {
+      redirect: 'follow',
+    })
+    const data = await res.text()
+
+    // remove the footer & get the relevant bits
+    const fakeDOM = document.createElement('html')
+    fakeDOM.innerHTML = data
+    fakeDOM.querySelector('h1:first-child').remove() // the name of the repo
+    fakeDOM.querySelector('.footer').remove() // a github message
+
+    const header = fakeDOM.querySelector('h1:first-child').innerText
+    fakeDOM.querySelector('h1:first-child').remove() // remove the header we just extracted into the header component
+    const body = fakeDOM.querySelector('body div:first-child').innerHTML
+
+    pageCache[url] = {
+      header,
+      body,
+    }
+  }
+
+  return pageCache[url]
+}
+
+const Guidebook = () => {
+  const [html, setHtml] = useState('')
+
+  // pulls from guidebook server whenever location updates
+  const location = useLocation()
+  useEffect(() => {
+    const fetchData = async () => {
+      setHtml(await fetchPage(location.pathname))
+    }
+    fetchData()
+  }, [location])
+
+  return (
+    <View style={styles.wrapper}>
+      <Header title={html.header} />
+      <LinkedScroll>
+        <View style={styles.innerWrapper}>
+          <div
+            className="guidebook-styles"
+            dangerouslySetInnerHTML={{ __html: html.body }}
+          />
+        </View>
+      </LinkedScroll>
+    </View>
+  )
+}
+
+export default Guidebook
