18891da6413aa193c3e98a4afa9485df76802367
Jono Cooper <jono@jonocooper.com>
2020-02-15 17:54:08 +1300

    Lock Provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -15,8 +15,9 @@ data "digitalocean_kubernetes_cluster" "sfo2" {
 }
 
 provider "kubernetes" {
-  host  = data.digitalocean_kubernetes_cluster.sfo2.endpoint
-  token = data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
+  version = "= 1.10.0"
+  host    = data.digitalocean_kubernetes_cluster.sfo2.endpoint
+  token   = data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.sfo2.kube_config[0].cluster_ca_certificate
   )
