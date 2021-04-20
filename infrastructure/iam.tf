7850e9df9025792465fc4c0fa2b9ff642e515f41
Jono Cooper <jono@jonocooper.com>
2021-04-20 19:52:12 +1200

    Remove ECS Infra

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,43 +1,4 @@
-resource "aws_iam_role" "api_service" {
-  name = "ecs-${var.name}-${data.aws_region.current.name}"
-
-  assume_role_policy = data.aws_iam_policy_document.api_service_assume.json
-}
-
-data "aws_iam_policy_document" "api_service_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_policy" "api_service" {
-  name        = "ecs-${var.name}-${data.aws_region.current.name}"
-  description = "Allow write to CloudWatch"
-  path        = "/"
-  policy      = data.aws_iam_policy_document.api_service.json
-}
-
 data "aws_iam_policy_document" "api_service" {
-  statement {
-    sid = "WriteAccessToCloudWatch"
-
-    actions = [
-      "cloudwatch:PutMetricData",
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:DescribeLogGroups",
-      "logs:DescribeLogStreams",
-      "logs:PutLogEvents",
-    ]
-
-    resources = ["*"]
-  }
-
   statement {
     sid = "PublishToSns"
 
@@ -49,9 +10,4 @@ data "aws_iam_policy_document" "api_service" {
       aws_sns_topic.feedback_notifications.arn
     ]
   }
-}
-
-resource "aws_iam_role_policy_attachment" "api_service" {
-  role       = aws_iam_role.api_service.name
-  policy_arn = aws_iam_policy.api_service.arn
 }
